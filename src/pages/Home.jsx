cc894a24b8c839dcc648a5749da800a83e274b7c
danielzm05 <danielzmfcbarcelona@gmail.com>
2024-09-16 19:46:59 -0300

    Nuevo diseño de la landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import "../styles/Home.css";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
-import { IconArrowRight } from "@tabler/icons-react";
+import { IconArrowRight, IconBallFootball, IconMountain, IconRun } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
+import campfire from "../img/campfire.gif";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -17,7 +18,7 @@ export function Home() {
     <>
       <Header />
       <main className="home-page">
-        <section className="home-section">
+        <section className="first-section">
           <div className="background"></div>
 
           <footer data-aos="fade-up" data-aos-duration="1000">
@@ -33,10 +34,20 @@ export function Home() {
           </footer>
         </section>
 
-        <section className="home-section">
+        <section className="categories-section">
           <h2 data-aos="zoom-in-up">FABRICANTES DESDE 1988</h2>
           <div className="categories">
+            <article className="category" data-aos="fade-down" data-aos-duration="1500">
+              <IconRun size={35} />
+              <h3>Running</h3>
+              <p>
+                Zapatillas de alto rendimiento, ropa técnica adaptable a cualquier clima, y una variedad de accesorios deportivos como relojes y
+                botellas. Todo lo que necesitas para alcanzar tus metas de running, independientemente del terreno o las condiciones.
+              </p>
+            </article>
+
             <article className="category" data-aos="fade-down" data-aos-duration="1000">
+              <IconMountain size={35} />
               <h3>Camping</h3>
               <p>
                 Diseñado para los aventureros que valoran la comodidad y la funcionalidad al aire libre. Ofrecemos una amplia gama de equipos, desde
@@ -45,15 +56,8 @@ export function Home() {
               </p>
             </article>
 
-            <article className="category" data-aos="fade-down" data-aos-duration="1500">
-              <h3>Running</h3>
-              <p>
-                Zapatillas de alto rendimiento, ropa técnica adaptable a cualquier clima, y una variedad de accesorios deportivos como relojes y
-                botellas. Todo lo que necesitas para alcanzar tus metas de running, independientemente del terreno o las condiciones.
-              </p>
-            </article>
-
             <article className="category" data-aos="fade-down" data-aos-duration="2000">
+              <IconBallFootball size={35} />
               <h3>Deportes</h3>
               <p>
                 Equipos y accesorios de alta calidad para una variedad de deportes, desde raquetas de tenis y pelotas hasta redes de voleibol. Nos
@@ -63,8 +67,16 @@ export function Home() {
           </div>
         </section>
 
-        <section className="home-section">
-          <article data-aos="fade-right" data-aos-offset="300" data-aos-easing="ease-in-sine">
+        <section className="camping">
+          <div data-aos="fade-up" className="img">
+            <img className="campfire" src="https://www.icegif.com/wp-content/uploads/2022/09/icegif-1610.gif" alt="campfire" />
+          </div>
+          <h2 data-aos="zoom-in-up">PRODUCTOS QUE TE ACOMPAÑAN EN TODOS TUS VIAJES</h2>
+        </section>
+
+        <section className="about">
+          <div className="image"></div>
+          <article data-aos="fade-down" data-aos-offset="300" data-aos-easing="ease-in-sine">
             <h2>¿Quiénes Somos?</h2>
             <p>
               Somos una compañía líder en la venta de equipamientos deportivos. Desde el 1998, trabajamos incansablemente para que nuestros clientes
@@ -74,7 +86,6 @@ export function Home() {
               <b> alta resistencia y durabilidad</b>.
             </p>
           </article>
-          <div className="image"></div>
         </section>
       </main>
       <Footer />
