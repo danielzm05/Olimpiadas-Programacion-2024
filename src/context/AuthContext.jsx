2776f64b55b3ffa8aa436be80d2622a3754f997e
danielzm05 <danielzmfcbarcelona@gmail.com>
2025-01-06 09:44:31 -0300

    Agregar registro de datos de usuario en AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -30,7 +30,7 @@ export const AuthProvider = ({ children }) => {
 
   const checkUser = async () => {
     const { data } = await supabase.auth.getUser();
-
+    console.log(data);
     if (data.user) {
       setUser(data.user);
       getUserInfo(data.user.id);
